10495f5d3ac4588f97373d8cde756d288a01af48
Jeong Jin Ung <jinung.dev@gmail.com>
2022-08-08 23:32:50 +0900

    Initial commit

---
+++ src/test/java/me/jinung/oop/RealWorldSoftwareDevelopmentApplicationTests.java
@@ -0,0 +1,13 @@
+package me.jinung.oop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealWorldSoftwareDevelopmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
